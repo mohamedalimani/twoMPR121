f96dfcbf082e27a0a348a7a1c457e3a22933ffd3
Manix <52538269+mohamedalimani@users.noreply.github.com>
2021-08-30 17:58:05 +0100

    read, decode and write data to log file
    need to install pySerial library in order to use this code

--- v1.py
+++ v1.py
@@ -1,9 +1,29 @@
-import serial 
-import time
+# u need to import pySerial to use this code 
+import serial
+import os.path
 
+#entities declaration 
 arduino = serial.Serial(port='COM3',baudrate=9600,timeout=.1)
+pointId = 1
+title = "history"
+fileId = 1 
+fileName = "history1.txt"
+
+# generate new log file with different name than the previous ones
+while os.path.isfile(fileName):
+	fileId = fileId + 1
+	fileName = title + str(fileId) + ".txt"
+f = open(fileName,"x")
+
+#read, decode and write data sent via serial into log file 
+f.write("\nUSER TOUCHED THE SENSORS AT THESE COORDINATES :\n")
+f.write("-------------------------------------------------\n")
+f.write("point n: X Y \n")
+f.write("-------------------------------------------------\n")
 while True:
 	data = arduino.readline()
 	x = data.decode("utf-8")
 	if len(x) > 1:
+		f.write("point "+str(pointId)+": "+str(x))
 		print(x)
+		pointId += 1
